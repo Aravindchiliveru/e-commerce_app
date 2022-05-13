e84965a30700bd7d72fc827f919e64a63f776125
Chiliveru-Aravind <102401493+Chiliveru-Aravind@users.noreply.github.com>
2022-05-13 14:02:41 +0530

    why

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -1,3 +1,5 @@
+import { HeartIcon,PlusIcon } from '@heroicons/react/outline'
+
 export const products = [
         
     {
@@ -385,13 +387,16 @@ export const products = [
           <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             {products.map((product) => (
               <div key={product.title} className="group relative">
-                <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-80 lg:h-80 lg:aspect-none">
                   <img
                     src={product.imageUrl}
                     alt={product.heading}
                     className="w-full h-full object-center object-cover lg:w-full lg:h-full"
                   />
+                  
                 </div>
+                <HeartIcon className='text-white h-[30px] absolute justify-center m-auto right-5 top-5 sm:top-0 sm:right-2 z-10 hover:cursor-grab '  />
+                 <PlusIcon className='text-white h-[30px] absolute justify-center m-auto right-5 top-20 sm:top-12 sm:right-2 z-10 hover:cursor-grab '/>
                 <div className="mt-4 flex justify-between">
                   <div>
                     <h3 className="text-sm text-gray-700">
