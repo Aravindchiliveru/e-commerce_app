6a4fc2ba35cfb574c04472fa15cbb0d3b949047a
Chiliveru-Aravind <102401493+Chiliveru-Aravind@users.noreply.github.com>
2022-05-16 03:07:22 +0530

    changes lill bit

--- components/Header.js
+++ components/Header.js
@@ -2,11 +2,12 @@ import React from 'react'
 import Image from 'next/image'
 import logo from '../images/logo.png'
 import { ShoppingCartIcon, UserIcon, SearchIcon,HeartIcon} from '@heroicons/react/outline'
+import Link from 'next/link'
 
 function Header({placeholder}) {
     placeholder = '';
   return (
-        <header className='top-0 sticky my-0 mx-0 z-50 grid grid-cols-5 bg-white p-5 md:px-10 shadow-md'>
+        <header className='top-0 w-screen sticky my-0 mx-0 z-50 grid grid-cols-5 bg-white p-5 md:px-10 shadow-md'>
 
         <div className='relative flex items-center h-10 cursor-pointer my-auto'>
           <Image src={logo} layout='fill' objectFit='contain' objectPosition='left'/>
@@ -19,13 +20,17 @@ function Header({placeholder}) {
           <SearchIcon className='hidden md:inline-flex h-8 hover:bg-black hover:text-white cursor-pointer p-2 rounded-full' />
         </div>
 
-
+        <Link href='/ExtraLargeCard'>
         <div className='flex items-center justify-end cursor-pointer' >
+          <div>
           <ShoppingCartIcon className=' h-7 hover:opacity-50'  />
         </div>
-
+          <p className='h-5  text-sm text-center rounded-full w-4 bg-violet-500 '>0</p>
+          </div>
+          </Link>
         <div className='flex items-center justify-end cursor-pointer '>
           <HeartIcon className='h-7 hover:opacity-50' />
+        <p className='h-5  text-sm text-center rounded-full w-4 bg-violet-500 '>0</p>
         </div>
 
         <div className='flex items-center justify-end cursor-pointer'>
